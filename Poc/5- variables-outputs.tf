060db23f7962ecba09540ab53f9b48b42dfa553a
bdairshadi <bdairshadi@gmail.com>
2021-10-24 02:52:40 +0300

    Updated the name of the environment

--- Poc/5- variables-outputs.tf
+++ Poc/5- variables-outputs.tf
@@ -179,25 +179,25 @@ output "web_lb_frontend_ip_configuration" {
 output "web_linuxvm_private_ip_address_list" {
   description = "Web Linux Virtual Machine Private IP"
   # 1 Way 
-  value = [for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.private_ip_address]
+  value = [for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.private_ip_address]
 }
 
 # Output Map  - Single Input to for loop
 output "web_linuxvm_private_ip_address_map" {
   description = "Web Linux Virtual Machine Private IP"
   # 2 Way
-  value = {for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address}
+  value = {for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address}
 }
 
 # Terraform keys() function: keys takes a map and returns a list containing the keys from that map.
 output "web_linuxvm_private_ip_address_keys_function" {
   description = "Web Linux Virtual Machine Private IP"
-  value = keys({for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address})
+  value = keys({for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address})
 }
 # Terraform values() function: values takes a map and returns a list containing the values of the elements in that map.
 output "web_linuxvm_private_ip_address_values_function" {
   description = "Web Linux Virtual Machine Private IP"
-  value = values({for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address})
+  value = values({for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.name => vm.private_ip_address})
 }
 
 # Output List - Two Inputs to for loop (here vm is Iterator like "i")
@@ -215,14 +215,14 @@ output "web_linuxvm_network_interface_id_map" {
 # Output Admin web VM's
 output "web_Admin_vm" {
   description = "Web Linux VM Admin"
-  value = [for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.admin_username]
+  value = [for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.admin_username]
 }
 
 # Output Password web VM's
 output "web_Password_vm" {
   description = "Web Linux VM Password"
   sensitive   = true
-  value = [for vm in azurerm_linux_virtual_machine.web_linuxvm: vm.admin_password]
+  value = [for vm in azurerm_windows_virtual_machine.web_linuxvm: vm.admin_password]
 }
 
 
