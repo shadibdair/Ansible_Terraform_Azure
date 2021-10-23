95e3282c3c8ba33d91f419ac39578a75e6e98c87
bdairshadi <bdairshadi@gmail.com>
2021-10-23 07:29:04 +0300

    Resize the vm machine to B2s

--- Production/2- web-tier.tf
+++ Production/2- web-tier.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
   name = "${local.resource_name_prefix}-web-${each.key}"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
-  size = "Standard_B1s"
+  size = "Standard_B2s"
   disable_password_authentication = "false"
   admin_username = "web-server-${each.key}"
   admin_password = var.web_password
